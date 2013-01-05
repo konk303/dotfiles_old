be1c2c18d4146ace7762e4b6873fb81458f67d52
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2013-01-05 10:09:09 +0900

    added emacs.d

---
+++ emacs.d/init.el
@@ -0,0 +1,5 @@
+;; Google IME on mac
+(setq default-input-method "MacOSX")
+(mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `title "漢")
+(mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `cursor-type `box)
+(mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `cursor-color "magenta")
